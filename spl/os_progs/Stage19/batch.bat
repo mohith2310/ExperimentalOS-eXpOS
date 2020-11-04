f532e7e5bfc128b1cddc35f3515e66fefa6168ce
mohith2310 <mohith23102000@gmail.com>
2020-11-04 16:06:17 +0530

    Stage 19 Done

---
+++ spl/os_progs/Stage19/batch.bat
@@ -0,0 +1,20 @@
+load --os $HOME/myexpos/spl/os_progs/Stage19/os_start.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --exhandler $HOME/myexpos/spl/os_progs/Stage19/exception_handler.xsm
+load --idle $HOME/myexpos/spl/os_progs/Stage19/idle.xsm
+load --init $HOME/myexpos/spl/os_progs/Stage19/shell1.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage19/linked.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage19/even.xsm
+load --module 0 $HOME/myexpos/spl/os_progs/Stage19/module0.xsm
+load --module 1 $HOME/myexpos/spl/os_progs/Stage19/module1.xsm
+load --module 2 $HOME/myexpos/spl/os_progs/Stage19/module2.xsm
+load --module 4 $HOME/myexpos/spl/os_progs/Stage19/module4.xsm
+load --module 5 $HOME/myexpos/spl/os_progs/Stage19/module5.xsm
+load --module 7 $HOME/myexpos/spl/os_progs/Stage19/module7.xsm
+load --int=6 $HOME/myexpos/spl/os_progs/Stage19/int6.xsm
+load --int=7 $HOME/myexpos/spl/os_progs/Stage19/int7.xsm
+load --int=9 $HOME/myexpos/spl/os_progs/Stage19/int9.xsm
+load --int=10 $HOME/myexpos/spl/os_progs/Stage19/int10.xsm
+load --int=timer $HOME/myexpos/spl/os_progs/Stage19/timer_int.xsm
+load --int=console $HOME/myexpos/spl/os_progs/Stage19/console_int.xsm
+load --int=disk $HOME/myexpos/spl/os_progs/Stage19/disk_int.xsm
