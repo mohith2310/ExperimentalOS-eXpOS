8b06e0517aa2a819209d56de98769fe0559c7e8f
mohith2310 <mohith23102000@gmail.com>
2020-11-12 12:29:52 +0530

    Bugs fixed in Stage 20

--- spl/os_progs/Stage20/batch.bat
+++ spl/os_progs/Stage20/batch.bat
@@ -5,17 +5,17 @@ load --idle $HOME/myexpos/spl/os_progs/Stage20/idle.xsm
 load --init $HOME/myexpos/spl/os_progs/Stage20/assg2.xsm
 load --exec $HOME/myexpos/spl/os_progs/Stage20/odd.xsm
 load --exec $HOME/myexpos/spl/os_progs/Stage20/even.xsm
-load --module 0 $HOME/myexpos/spl/os_progs/Stage20/resource_manager_module.xsm
-load --module 1 $HOME/myexpos/spl/os_progs/Stage20/process_man_mod.xsm
-load --module 2 $HOME/myexpos/spl/os_progs/Stage20/memory_man_mod.xsm
-load --module 4 $HOME/myexpos/spl/os_progs/Stage20/device_manager_module.xsm
-load --module 5 $HOME/myexpos/spl/os_progs/Stage20/schedular_module.xsm
-load --module 7 $HOME/myexpos/spl/os_progs/Stage20/boot_module.xsm
-load --int=6 $HOME/myexpos/spl/os_progs/Stage20/read_int.xsm
-load --int=7 $HOME/myexpos/spl/os_progs/Stage20/write_int.xsm
-load --int=8 $HOME/myexpos/spl/os_progs/Stage20/fork_int.xsm
-load --int=9 $HOME/myexpos/spl/os_progs/Stage20/exec_int.xsm
-load --int=10 $HOME/myexpos/spl/os_progs/Stage20/exit_int.xsm
+load --module 0 $HOME/myexpos/spl/os_progs/Stage20/module0.xsm
+load --module 1 $HOME/myexpos/spl/os_progs/Stage20/module1.xsm
+load --module 2 $HOME/myexpos/spl/os_progs/Stage20/module2.xsm
+load --module 4 $HOME/myexpos/spl/os_progs/Stage20/module4.xsm
+load --module 5 $HOME/myexpos/spl/os_progs/Stage20/module5.xsm
+load --module 7 $HOME/myexpos/spl/os_progs/Stage20/module7.xsm
+load --int=6 $HOME/myexpos/spl/os_progs/Stage20/int6.xsm
+load --int=7 $HOME/myexpos/spl/os_progs/Stage20/int7.xsm
+load --int=8 $HOME/myexpos/spl/os_progs/Stage20/int8.xsm
+load --int=9 $HOME/myexpos/spl/os_progs/Stage20/int9.xsm
+load --int=10 $HOME/myexpos/spl/os_progs/Stage20/int10.xsm
 load --int=timer $HOME/myexpos/spl/os_progs/Stage20/timer_int.xsm
 load --int=console $HOME/myexpos/spl/os_progs/Stage20/console_int.xsm
 load --int=disk $HOME/myexpos/spl/os_progs/Stage20/disk_int.xsm
