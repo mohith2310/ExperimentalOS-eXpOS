9d647b827ab599812d07705858723f79e5e804e8
mohith2310 <mohith23102000@gmail.com>
2020-11-28 14:29:45 +0530

    Stage 24 Done

---
+++ spl/os_progs/Stage24/batch.bat
@@ -0,0 +1,30 @@
+load --os $HOME/myexpos/spl/os_progs/Stage24/os_start.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --exhandler $HOME/myexpos/spl/os_progs/Stage24/excepHandler.xsm
+load --idle $HOME/myexpos/spl/os_progs/Stage24/idle.xsm
+load --init $HOME/myexpos/spl/os_progs/Stage24/shell2.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage24/assg1.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage24/assg2.xsm
+load --data $HOME/myexpos/spl/os_progs/Stage24/number.dat
+load --data $HOME/myexpos/spl/os_progs/Stage24/sample.dat
+load --module 0 $HOME/myexpos/spl/os_progs/Stage24/module0.xsm
+load --module 1 $HOME/myexpos/spl/os_progs/Stage24/module1.xsm
+load --module 2 $HOME/myexpos/spl/os_progs/Stage24/module2.xsm
+load --module 3 $HOME/myexpos/spl/os_progs/Stage24/module3.xsm
+load --module 4 $HOME/myexpos/spl/os_progs/Stage24/module4.xsm
+load --module 5 $HOME/myexpos/spl/os_progs/Stage24/module5.xsm
+load --module 7 $HOME/myexpos/spl/os_progs/Stage24/module7.xsm
+load --int=4 $HOME/myexpos/spl/os_progs/Stage24/int4.xsm
+load --int=5 $HOME/myexpos/spl/os_progs/Stage24/int5.xsm
+load --int=6 $HOME/myexpos/spl/os_progs/Stage24/int6.xsm
+load --int=7 $HOME/myexpos/spl/os_progs/Stage24/int7.xsm
+load --int=8 $HOME/myexpos/spl/os_progs/Stage24/int8.xsm
+load --int=9 $HOME/myexpos/spl/os_progs/Stage24/int9.xsm
+load --int=10 $HOME/myexpos/spl/os_progs/Stage24/int10.xsm
+load --int=11 $HOME/myexpos/spl/os_progs/Stage24/int11.xsm
+load --int=13 $HOME/myexpos/spl/os_progs/Stage24/int13.xsm
+load --int=14 $HOME/myexpos/spl/os_progs/Stage24/int14.xsm
+load --int=15 $HOME/myexpos/spl/os_progs/Stage24/int15.xsm
+load --int=timer $HOME/myexpos/spl/os_progs/Stage24/timer_int.xsm
+load --int=console $HOME/myexpos/spl/os_progs/Stage24/console_int.xsm
+load --int=disk $HOME/myexpos/spl/os_progs/Stage24/disk_int.xsm
