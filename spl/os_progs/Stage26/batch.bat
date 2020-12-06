354afe1cff21cebdf5f2857bfe78dc3208251016
mohith2310 <mohith23102000@gmail.com>
2020-12-06 17:38:54 +0530

    Stage 26 Done

---
+++ spl/os_progs/Stage26/batch.bat
@@ -0,0 +1,44 @@
+load --os $HOME/myexpos/spl/os_progs/Stage26/os_start.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --exhandler $HOME/myexpos/spl/os_progs/Stage26/excep_handler.xsm
+load --idle $HOME/myexpos/spl/os_progs/Stage26/idle.xsm
+load --init $HOME/myexpos/spl/os_progs/Stage26/login.xsm
+load --shell $HOME/myexpos/spl/os_progs/Stage26/shell.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/odd.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/even.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/pid.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/create.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/delete.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/f_write.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/cat.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/ls.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/rm.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/cp.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/lu.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage26/ru.xsm
+load --data $HOME/myexpos/spl/os_progs/Stage26/sample.dat
+load --data $HOME/myexpos/spl/os_progs/Stage26/numbers.dat
+load --module 0 $HOME/myexpos/spl/os_progs/Stage26/module0.xsm
+load --module 1 $HOME/myexpos/spl/os_progs/Stage26/module1.xsm
+load --module 2 $HOME/myexpos/spl/os_progs/Stage26/module2.xsm
+load --module 3 $HOME/myexpos/spl/os_progs/Stage26/module3.xsm
+load --module 4 $HOME/myexpos/spl/os_progs/Stage26/module4.xsm
+load --module 5 $HOME/myexpos/spl/os_progs/Stage26/module5.xsm
+load --module 7 $HOME/myexpos/spl/os_progs/Stage26/module7.xsm
+load --int=4 $HOME/myexpos/spl/os_progs/Stage26/int4.xsm
+load --int=5 $HOME/myexpos/spl/os_progs/Stage26/int5.xsm
+load --int=6 $HOME/myexpos/spl/os_progs/Stage26/int6_read.xsm
+load --int=7 $HOME/myexpos/spl/os_progs/Stage26/int7_write.xsm
+load --int=8 $HOME/myexpos/spl/os_progs/Stage26/int8_fork.xsm
+load --int=9 $HOME/myexpos/spl/os_progs/Stage26/int9_exec.xsm
+load --int=10 $HOME/myexpos/spl/os_progs/Stage26/int10_exit.xsm
+load --int=11 $HOME/myexpos/spl/os_progs/Stage26/int11.xsm
+load --int=12 $HOME/myexpos/spl/os_progs/Stage26/int12.xsm
+load --int=13 $HOME/myexpos/spl/os_progs/Stage26/int13.xsm
+load --int=14 $HOME/myexpos/spl/os_progs/Stage26/int14.xsm
+load --int=15 $HOME/myexpos/spl/os_progs/Stage26/int15_shutdown.xsm
+load --int=16 $HOME/myexpos/spl/os_progs/Stage26/int16.xsm
+load --int=17 $HOME/myexpos/spl/os_progs/Stage26/int17.xsm
+load --int=timer $HOME/myexpos/spl/os_progs/Stage26/int_timer.xsm
+load --int=console $HOME/myexpos/spl/os_progs/Stage26/int_console.xsm
+load --int=disk $HOME/myexpos/spl/os_progs/Stage26/int_disk.xsm
