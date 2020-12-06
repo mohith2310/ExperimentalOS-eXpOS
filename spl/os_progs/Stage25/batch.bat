4fa26e3f92b49857cb8ea30a69b79d151777eca4
mohith2310 <mohith23102000@gmail.com>
2020-12-06 17:38:21 +0530

    Stage 25 Done

---
+++ spl/os_progs/Stage25/batch.bat
@@ -0,0 +1,31 @@
+load --os $HOME/myexpos/spl/os_progs/Stage25/os_start.xsm
+load --library $HOME/myexpos/expl/library.lib
+load --exhandler $HOME/myexpos/spl/os_progs/Stage25/excep_handler.xsm
+load --idle $HOME/myexpos/spl/os_progs/Stage25/idle.xsm
+load --init $HOME/myexpos/spl/os_progs/Stage25/shell.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage25/assg1.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage25/assg2.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage25/assg3.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage25/assg4.xsm
+load --exec $HOME/myexpos/spl/os_progs/Stage25/cat.xsm
+load --module 0 $HOME/myexpos/spl/os_progs/Stage25/module0.xsm
+load --module 1 $HOME/myexpos/spl/os_progs/Stage25/module1.xsm
+load --module 2 $HOME/myexpos/spl/os_progs/Stage25/module2.xsm
+load --module 3 $HOME/myexpos/spl/os_progs/Stage25/module3.xsm
+load --module 4 $HOME/myexpos/spl/os_progs/Stage25/module4.xsm
+load --module 5 $HOME/myexpos/spl/os_progs/Stage25/module5.xsm
+load --module 7 $HOME/myexpos/spl/os_progs/Stage25/module7.xsm
+load --int=4 $HOME/myexpos/spl/os_progs/Stage25/int4.xsm
+load --int=5 $HOME/myexpos/spl/os_progs/Stage25/int5.xsm
+load --int=6 $HOME/myexpos/spl/os_progs/Stage25/int6_read.xsm
+load --int=7 $HOME/myexpos/spl/os_progs/Stage25/int7_write.xsm
+load --int=8 $HOME/myexpos/spl/os_progs/Stage25/int8_fork.xsm
+load --int=9 $HOME/myexpos/spl/os_progs/Stage25/int9_exec.xsm
+load --int=10 $HOME/myexpos/spl/os_progs/Stage25/int10_exit.xsm
+load --int=11 $HOME/myexpos/spl/os_progs/Stage25/int11.xsm
+load --int=13 $HOME/myexpos/spl/os_progs/Stage25/int13.xsm
+load --int=14 $HOME/myexpos/spl/os_progs/Stage25/int14.xsm
+load --int=15 $HOME/myexpos/spl/os_progs/Stage25/int15_shutdown.xsm
+load --int=timer $HOME/myexpos/spl/os_progs/Stage25/int_timer.xsm
+load --int=console $HOME/myexpos/spl/os_progs/Stage25/int_console.xsm
+load --int=disk $HOME/myexpos/spl/os_progs/Stage25/int_disk.xsm
